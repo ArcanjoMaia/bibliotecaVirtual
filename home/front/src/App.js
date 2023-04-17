543b7632fafda0ba86bbef7e01c28a5f22844708
Lucas <lucasmaialol1@hotmail.com>
2023-04-17 20:50:32 -0300

    Iniciando o Front

---
+++ home/front/src/App.js
@@ -0,0 +1,13 @@
+/* Pagina inicial */
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Judasteca</a>
+      </header>
+      <main>lista de produtos</main>
+    </div>
+  );
+}
+
+export default App;
