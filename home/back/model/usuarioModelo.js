c3d9fecaceb1a0f0695a40deeed0430e48bb1564
lolasilvag <lolasilva.25@gmail.com>
2023-04-21 20:18:40 -0300

    Create usuarioModelo.js

---
+++ home/back/model/usuarioModelo.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+/* Cadastro de usuario do site */
+const userSchema = new mongoose.Schema(
+  {
+    nome: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    senha: { type: String, required: true },
+    resetToken: { type: String },
+    admin: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
