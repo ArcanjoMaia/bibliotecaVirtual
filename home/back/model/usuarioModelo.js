f4c2eb487ba5b6c4dcaab75a82d07b1307690c05
lolasilvag <lolasilva.25@gmail.com>
2023-04-22 13:54:47 -0300

    Update usuarioModelo.js

--- home/back/model/usuarioModelo.js
+++ home/back/model/usuarioModelo.js
@@ -14,5 +14,5 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model('User', userSchema);
-export default User;
+const Usuario = mongoose.model('Usuario', userSchema);
+export default Usuario;
